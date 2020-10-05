4ca05fb79ab687627178a856c84d2aac47109ba8
AlexValder <angelaarhangelska@gmail.com>
2020-10-05 22:29:33 +0300

    Trying to make a table

---
+++ lab01/graphics.py
@@ -0,0 +1,11 @@
+from prettytable import PrettyTable
+from typing import List, Tuple, Callable
+
+
+# Table: x_k, y(x_k), y_k, y(x_k) - y_k,
+
+def print_table(pairs: List[Tuple[float, float]], f: Callable[[float, float], float]) -> None:
+	t = PrettyTable(["X_k", "Y(X_k)", "Y_k", "Y(X_k) - Y_k"])
+	for x, y in pairs:
+		t.add_row([f'{x}', f'{f(x)}', f'{y}', f'{f(x) - y}'])
+	print(t)
--- lab01/main.py
+++ lab01/main.py
@@ -15,22 +15,23 @@
 import numpy as np
 from typing import Callable, List, Tuple
 import automatic as at
+import graphics as gr
 
 
 def funct(x: float, y: float) -> float:
 	return x*y**3 - 1
 
 
+x_0: float = 0.0
+y_0: float = 0.0
 a: float = 0.0
 b: float = 1.0
 h_0: float = 0.5
 
 if __name__ == "__main__":
 
-	auto_pairs : List[Tuple[float, float]] = [(0.0, 0.0)]
+	auto_pairs : List[Tuple[float, float]] = [(x_0, y_0)]
 
 	auto_pairs = at.get_pairs(auto_pairs, funct, a, b, h_0)
 
-	print("VALUES:")
-	for x, y in auto_pairs:
-		print(f'x = {x}\t\ty = {y}')
+	gr.print_table(auto_pairs, funct)
